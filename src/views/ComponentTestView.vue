72eba151781d93da18289e212894f6a9d17b7ab5
Erik Andres Felea <erikandresfelea@gmail.com>
2022-12-16 12:40:53 +0100

    Testing out vue

---
+++ src/views/ComponentTestView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import WelcomeItem from '../components/WelcomeItem.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+    <h3>So many components...</h3>
+    <TheWelcome />
+    <h3>Actualy too many...</h3>
+    <WelcomeItem />
+    <WelcomeItem />
+    <WelcomeItem />
+    <WelcomeItem />
+    <WelcomeItem />
+    <h3>Please stop</h3>
+    <HelloWorld />
+    <TheWelcome />
+  </main>
+</template>
